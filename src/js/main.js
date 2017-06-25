17053e0b3540d8e851f50cbf84b9a8d3a34090b8
Nathachai Thongniran <inid3a@gmail.com>
2017-06-25 18:09:19 +0700

    fix(resize): Empty canvas when resize on game pause state

--- src/js/main.js
+++ src/js/main.js
@@ -339,6 +339,8 @@ function initListener() {
   // https://css-tricks.com/debouncing-throttling-explained-examples/
   // https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
   window.addEventListener('resize', _.debounce(function() {
+    // hack (tmp fix)
+    if (isPause) return;
     updateCanvasSize();
   }, 200));
 }
